92c4c619c408dd88816aac19e0f4015cdeef7ed7
Allen Ling <allenling3@gmail.com>
2019-10-26 15:40:00 +0800

    Update longest_substring_without_repeat.py

--- medium/longest_substring_without_repeat.py
+++ medium/longest_substring_without_repeat.py
@@ -1,72 +1,82 @@
 '''
-https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+longest_substring_without_repeating_characters
+无重复字符的最长子串
 
-动态规划, 遍历一次, 使用中间变量保存结果, O(n)
 
-第一次才领先14%~~~尴尬
+主要是动态规划的思路, 肯定能O(n)的
 
-第二次是领先36%, 差不多感觉
+思路, 其中x为元素, x1表示第一个x, x2表示第二个x
 
-2018-06-02再修改, 想了一个流程, 然后自己也看不懂了, 然后过了leetcode, 领先83.55%, 耗时88ms
+left
+    ---x1---------x2-------x3---------
 
-最快68ms, 我这里中间变量用得多, 耗时多一点点, 思路差不多
+left=0, 遇到了第二个x(x2), 它是重复的, 那么显然我们的最长子串就是left-(x2-1), 此时吧Left设置为x2+1
+
+                    left
+    ---x1---------x2-------x3---------
+
+然后继续, 如果我们遇到x3, 那么其实x3和x1重复我们不需要管, 因为x2和x1已经重复了, 也就是说left的位置说明的是
+
+                    left
+    ---x1---o2------x2-------o2---------
+
+left之前已经有重复元素了, 此时如果一个元o2素的重复元o1是在left之前, 那完全没有意义, 因为任何left之前的元素
+
+和left之后的元素, 是不能形成子串的, 因为left之前就有两个相同的元素打断了子串, 所以
+
+如果o1的位置是left之前, 没关系, 继续走, 此时我们的子串继续增长, 主要要记录元素o的最后位置是o2
+
+也就是说从left开始没遇到重复元素的子串, 此时我们遇到重复子串实在left之后, 说明当前子串被截断了, 要
+
+计算长度了
+
+所以最最重要的就是left位置的含义!!!!!!!!!!!!!!!!!
+
+执行用时 :
+48 ms, 在所有 python3 提交中击败了99.91%的用户
+
 '''
 
 
-def lswr(word):
-    '''
-    判断2部分, 也就是调整下面两部分的下标:
-    abcde bfgh
-    1. abcde
-    2. cdebfgh
-
-    下面的流程, 虽然是我自己想出来的, 但是我自己都看不懂了
-    '''
-    word_len = len(word)
-    if not word:
-        return 0
-    if word_len == 1:
-        return 1
-    if word_len == 2:
-        return 1 if word[0] == word[1] else 2
-    max_dict = {}
-    max_start = max_end = 0
-    split_index = None
-    for index, w in enumerate(word):
-        w = word[index]
-        old_index = max_dict.get(w, None)
-        max_dict[w] = index
-        if split_index is not None:
-            if old_index is None or old_index < split_index:
-                continue
-            else:
-                last_len = index - split_index
-                if last_len >= (max_end - max_start + 1):
-                    max_start, max_end = split_index, index - 1
-                    split_index = old_index + 1
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) <= 1:
+            return len(s)
+        left = 0
+        longest = 0
+        last = dict()
+        
+        for index, value in enumerate(s):
+            if value in last:
+                # 判断是否在left之后, 不在的话, 当前子串不需要被打断
+                if last[value] < left:
+                    pass
                 else:
-                    split_index = old_index + 1
-        else:
-            if old_index is None:
-                max_end = index
-            else:
-                split_index = old_index + 1
-    # finally
-    max_len = (max_end - max_start + 1)
-    if split_index is not None:
-        max_len = max(max_len, word_len - split_index)
-    return max_len
+                    # 如果是在left之后, 那么说明当前子串要被打断了, 计算子串的长度和longest比较吧
+                    # 注意的是, 子串被打断了, 那么子串的最长长度就是index - left!
+                    # 不要考虑重复元素
+                    sub_len = index - left
+                    if sub_len > longest:
+                        longest = sub_len
+                        # 重置left
+                    left = last[value] + 1
+            last[value] = index
+        # 考虑下一直走到最后都没有重复元素的话
+        if len(s) - left > longest:
+            longest = len(s) - left
+
+        return longest
+
 
 
 def main():
-    ss = ['bpoiexpqhmebhhu', 'pwwkew', 'abba', 'dvdf', 'au', 'abcabcbb', 'bbbbb']
+    ss = ["abcabcbb", "bbbb", "bcbbbb", "abcdefagp", "", "a"]
     for s in ss:
-        print(s, lswr(s))
-    print('-----------------')
-    for s in ss:
-        print(s, lswr(s))
+        x = Solution()
+        res = x.lengthOfLongestSubstring(s)
+        print(s, res)
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
